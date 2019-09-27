cd4233de19a466025aa711666d44fb8ec65a2608
pys200603027 <pys200603027@gmail.com>
2019-09-27 16:53:53 +0800

    update gradle

--- app/src/main/java/water/android/io/samplejni/MainActivity.java
+++ app/src/main/java/water/android/io/samplejni/MainActivity.java
@@ -1,6 +1,6 @@
 package water.android.io.samplejni;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
